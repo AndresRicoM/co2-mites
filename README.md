3a1621cc64e3e0b499bad6a27415ce1e1ebdeb16
AndresRicoM <aricom@mit.edu>
2020-11-03 17:44:04 -0500

    added timestamps

--- README.md
+++ README.md
@@ -4,8 +4,10 @@ Sensor fusion for determination of space occupancy.
 
 Test Data scripts:
 
-SDC30
+SDC30 - ID = 8360568
 http://replace.media.mit.edu:8080/TermitesV2/getsinglesensorbydateid2020.php?sensorid=8360568&start=2020-9-20-0-0-0-0&end=2020-09-21-20-0-0&project=universal
 
-S1
+S1 - ID = 8360837
 http://replace.media.mit.edu:8080/TermitesV2/getsinglesensorbydateid2020.php?sensorid=8360837&start=2020-9-20-0-0-0-0&end=2020-09-21-20-0-0&project=universal
+
+PIR - ID = ID:8362833
--- ml/load_script.py
+++ ml/load_script.py
@@ -3,13 +3,7 @@ import re
 import json
 import requests
 import numpy as np
-
-"""
-sensorId = "8360568"
-s = "2020-10-18"
-e = '2020-10-21'
-des = ['temperature', 'humidity', 'ambientLight', 'eco2']
-#"""
+import pandas as pd
 
 def queryTermiteServer(start, end, id, desiredVariables):
 
@@ -21,8 +15,11 @@ def queryTermiteServer(start, end, id, desiredVariables):
     termiteName = termiteData['name']
     termiteValues = termiteData['values']
     termiteTimes = termiteData['time']
+    termiteTimes = np.array(termiteTimes)
+    #print(termiteTimes.shape)
 
-    output_data = np.arange(len(termiteValues))
+    output_data = termiteTimes
+    #print(output_data.shape)
 
     for items in desiredVariables:
         holdingVector = np.zeros(len(termiteValues))
@@ -31,13 +28,33 @@ def queryTermiteServer(start, end, id, desiredVariables):
             try:
                 holdingVector[rows] = float(pHolder[items]) #Save value as a float on new NP array.
             except:
-                print('Key exception occured: Todo Sabroso no pasa nada! - AR')
+                print('Key exception occured: Todo Sabroso no pasa nada, besos y animo! - AR')
 
         output_data = np.vstack((output_data, holdingVector))
 
-    output_data = np.delete(output_data, 0, axis = 0)
+    #output_data = np.delete(output_data, 0, axis = 0)
 
     return  np.transpose(output_data) #np.any(np.isnan(output_data)), np.all(np.isfinite(output_data))
  #np.transpose(output_data)
 
-#print(queryTermiteServer(s, e, sensorId, des))
+def structure_pir(main_data, pir_data):
+    print(main_data.shape)
+    print(pir_data.shape)
+    return main_data
+
+sensorId = '8362833'
+s = '2020-10-20'
+e = '2020-11-1'
+des = ['pir']
+
+pir_data = queryTermiteServer(s, e, sensorId, des)
+
+sensorId = '8360568'
+s = '2020-10-20'
+e = '2020-11-1'
+des = ['eco2']
+
+main_data = queryTermiteServer(s, e, sensorId, des)
+
+print(type(main_data[0,0]))
+print(pir_data.shape)
