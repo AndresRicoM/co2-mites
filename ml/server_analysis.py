a5741663bcf4b58c5584390afdd21f5794b0092c
aricom <aricom@mit.edu>
2021-06-23 11:41:33 -0400

    added server query

---
+++ ml/server_analysis.py
@@ -0,0 +1,55 @@
+
+# -*- coding: UTF-8 -*-
+#
+#    ██████╗ ██████╗ ██████╗       ███╗   ███╗██╗████████╗███████╗███████╗
+#   ██╔════╝██╔═══██╗╚════██╗      ████╗ ████║██║╚══██╔══╝██╔════╝██╔════╝
+#   ██║     ██║   ██║ █████╔╝█████╗██╔████╔██║██║   ██║   █████╗  ███████╗
+#   ██║     ██║   ██║██╔═══╝ ╚════╝██║╚██╔╝██║██║   ██║   ██╔══╝  ╚════██║
+#   ╚██████╗╚██████╔╝███████╗      ██║ ╚═╝ ██║██║   ██║   ███████╗███████║
+#    ╚═════╝ ╚═════╝ ╚══════╝      ╚═╝     ╚═╝╚═╝   ╚═╝   ╚══════╝╚══════╝
+#
+#   ╔═╗┬┌┬┐┬ ┬  ╔═╗┌─┐┬┌─┐┌┐┌┌─┐┌─┐       ╔╦╗╦╔╦╗  ╔╦╗┌─┐┌┬┐┬┌─┐  ╦  ┌─┐┌┐
+#   ║  │ │ └┬┘  ╚═╗│  │├┤ ││││  ├┤   ───  ║║║║ ║   ║║║├┤  │││├─┤  ║  ├─┤├┴┐
+#   ╚═╝┴ ┴  ┴   ╚═╝└─┘┴└─┘┘└┘└─┘└─┘       ╩ ╩╩ ╩   ╩ ╩└─┘─┴┘┴┴ ┴  ╩═╝┴ ┴└─┘
+#
+#                                   .|
+#                                  | |
+#                                  |'|            ._____
+#                          ___    |  |            |.   |' .---"|
+#                  _    .-'   '-. |  |     .--'|  ||   | _|    |
+#               .-'|  _.|  |    ||   '-__  |   |  |    ||      |
+#               |' | |.    |    ||       | |   |  |    ||      |
+#            ___|  '-'     '    ""       '-'   '-.'    '`      |____
+#
+#
+
+import time
+import psycopg2
+import datetime
+
+connection = psycopg2.connect(user="blindtermite",
+                                  password = "BlindAsAB@t",
+                                  host="localhost",
+                                  port="5444",
+                                  database="termites")
+
+try:
+   cursor = connection.cursor()
+
+   #postgres_insert_query = """ INSERT INTO termxiteclusters (inserttime, co2,  pir, cluster, confidence, chipid) VALUES (%s,%s,%s,%s,%s,%s)"""
+   #cursor.execute(postgres_insert_query, data2send)
+
+   #connection.commit()
+   #count = cursor.rowcount
+   #print (count, "Record inserted successfully into mobile table")
+
+except (Exception, psycopg2.Error) as error :
+    if(connection):
+        print("Failed to insert record into termiteclusters table", error)
+
+finally:
+    #closing database connection.
+    if(connection):
+        cursor.close()
+        connection.close()
+        print("PostgreSQL connection is closed")
