ffd2ad13c078dabcb467963ab7e2eee0769d0e78
AndresRicoM <andres.rico.m@gmail.com>
2022-03-15 13:16:03 -0400

    validation

--- ml/evaluate_RNN.py
+++ ml/evaluate_RNN.py
@@ -73,7 +73,7 @@ model.add(Dropout(0.2))
 model.add(Dense(500, activation='relu'))
 model.add(Dropout(0.2))
 
-model.add(Dense(3, activation='softmax')) #Only One output Unit
+model.add(Dense(5, activation='softmax')) #Only One output Unit
 
 #Declare optimizing fucntion and parameters.
 opt = tf.keras.optimizers.Adam(lr=0.001, decay=1e-6)
